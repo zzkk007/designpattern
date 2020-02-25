3d092b2554d716b359b8fab4a5882a9df7b2840c
huanongzhangkun <zhangkun@chinahuanong.com.cn>
2020-02-25 22:03:52 +0800

    代码复用

---
+++ src/main/java/com/km/designpattern/geekbang/beautyDesign21/UserAuthenticator.java
@@ -0,0 +1,23 @@
+package com.km.designpattern.geekbang.beautyDesign21;
+
+/**
+ *  DRY 原则： Don't Repeat Yourself
+ *  三种典型的代码重复：实现逻辑重复、功能语义重复、代码执行重复。
+ *  实现逻辑一样，单语义不一样，我们能判断不违反 DRY 原则。
+ *  包含重复代码的问题，我们可以抽象成更细粒度函数的方式来解决。
+ *
+ *  代码复用性（Code Reusability）、代码复用（Code Resue）和 DRY 原则。
+ *  1、减少代码耦合
+ *  2、满足单一职责原则
+ *  3、模块化
+ *  4、业务与非业务逻辑分离
+ *  5、通用代码下沉
+ *  6、继承、多态、抽象、封装
+ *  7、应用模板等设计模式
+ */
+// 实现逻辑重复
+public class UserAuthenticator {
+
+
+
+}
