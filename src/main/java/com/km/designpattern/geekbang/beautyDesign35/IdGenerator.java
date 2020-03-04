a8b4c5d5b3dc85f17f96b256075139c2bb8bcd10
huanongzhangkun <zhangkun@chinahuanong.com.cn>
2020-03-04 23:15:55 +0800

    如何发现代码质量问题

---
+++ src/main/java/com/km/designpattern/geekbang/beautyDesign35/IdGenerator.java
@@ -0,0 +1,5 @@
+package com.km.designpattern.geekbang.beautyDesign35;
+
+public interface IdGenerator {
+    String generate();
+}
---
+++ src/main/java/com/km/designpattern/geekbang/beautyDesign35/LogTraceIdGenerator.java
@@ -0,0 +1,4 @@
+package com.km.designpattern.geekbang.beautyDesign35;
+
+public interface LogTraceIdGenerator extends IdGenerator {
+}
