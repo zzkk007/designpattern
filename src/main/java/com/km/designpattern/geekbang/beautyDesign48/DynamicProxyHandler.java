1a654eb3f643a7c304bad19468ca82ad7ef6fe2c
huanongzhangkun <zhangkun@chinahuanong.com.cn>
2020-04-06 23:22:55 +0800

    动态代理

--- src/main/java/com/km/designpattern/geekbang/beautyDesign48/DynamicProxyHandler.java
+++ src/main/java/com/km/designpattern/geekbang/beautyDesign48/DynamicProxyHandler.java
@@ -3,6 +3,17 @@ package com.km.designpattern.geekbang.beautyDesign48;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+/**
+ * InvocationHandler 是个接口，实现了InvocationHandler 接口的类用于加强目标类的主业务逻辑。
+ * 这个接口中有一个方法 invoke()，具体加强的代码逻辑就定义在改方法中。程序调用注业务逻辑时，会自动
+ * 自动调用该方法：
+ *
+ *  public Object invoke(object proxy, Method method, Object[] args)
+ *  proxy: 代表生产的代理对象
+ *  method: 代表目标方法
+ *  args: 代码目标方法的参数
+ *
+ */
 public class DynamicProxyHandler implements InvocationHandler {
 
     private Object proxiedObject;
--- src/main/java/com/km/designpattern/geekbang/beautyDesign48/MetricsCollectorProxy.java
+++ src/main/java/com/km/designpattern/geekbang/beautyDesign48/MetricsCollectorProxy.java
@@ -2,6 +2,21 @@ package com.km.designpattern.geekbang.beautyDesign48;
 
 import java.lang.reflect.Proxy;
 
+/**
+ *  使用JDK的 java.lang.reflect.Proxy 类实现动态代理，会使用其静态方法
+ *  newProxyInstance(), 依据目标对象，业务接口以及业务增强逻辑三者，自动生产一个动态代理对象
+ *
+ *  public static newProxyInstance(ClassLoader loader, Class<?>[] interfaces, InvocationHandler handler)
+ *
+ *  loader: 目标类的类加载器，通过目标类的反射可获取
+ *  interfaces: 目标类实现的接口数组，通过目标类的反射可获取
+ *  handler: 业务增强逻辑，需要再定义。
+ *
+ *
+ */
+
+
+
 public class MetricsCollectorProxy {
 
     private MetricsCollector metricsCollector;
@@ -14,6 +29,7 @@ public class MetricsCollectorProxy {
         // 够获得这个对象所实现的接口
         Class<?>[] interfaces = proxiedObject.getClass().getInterfaces();
 
+        // 业务增强
         DynamicProxyHandler handler = new DynamicProxyHandler(proxiedObject);
         return Proxy.newProxyInstance(proxiedObject.getClass().getClassLoader(), interfaces, handler);
     }
